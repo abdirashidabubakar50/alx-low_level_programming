bedc63c5c200169c8ccb6eb422a225f62e77095f
abdirashidabubakar7 <abdirashidabubakar7@penguin>
2023-06-22 22:46:06 +0300

    was not that much  thought

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - draw a triangle followed by a new line
+ * @size: the number of the times the character '#' is printed
+ */
+void print_triangle(int size)
+{
+	int row, column;
+
+	/* for each row*/
+	for (row = 0; row < size; row++)
+	{
+		/* for each column*/
+		for (column = 0; column < size; column++)
+		{
+			if (row + column < size - 1)
+			{
+				_putchar(' ');
+			}
+			/* print a # */
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
