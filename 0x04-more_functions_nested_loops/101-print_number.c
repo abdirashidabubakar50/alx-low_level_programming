9d82ead9a51f3b96e33beb5a714edceed1133f9e
abdirashidabubakar7 <abdirashidabubakar7@penguin>
2023-06-24 04:23:07 +0300

    this should be my last one commit for today

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,9 @@
  */
 void print_number(int n)
 {
+	int divisor = 1;
+	int digit;
+	int negative = 0;
 
 	if (n == 0)
 	{
@@ -16,13 +19,20 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		negative = 1;
 	}
 
-	if (n / 10)
+	while (n / divisor != 0)
+		divisor *= 10;
+
+	divisor /= 10;
+
+	while (divisor != 0)
 	{
-		print_number(n / 10);
+		digit = (n / divisor) % 10;
+		if (negative)
+			digit = -digit;
+		_putchar('0' + digit);
+		divisor /= 10;
 	}
-
-	_putchar((n % 10) + '0');
 }
