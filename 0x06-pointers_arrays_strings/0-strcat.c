99b4163b13e84c80fad2be7ee0b4d0edd4c227e5
abdirashidabubakar7 <abdirashidabubakar7@penguin>
2023-06-29 17:28:30 +0300

    concatenating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  *_strcat - a function that concatenates two strings
+ *  @dest: The destinations string
+ *  @src: The source string 
+ * 
+ * Return: poınter to the resultıng strıng dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int length_dest = 0;
+	int i;
+
+	/*determine the length of the destination string*/
+	while(dest[length_dest] != '\0')
+	{
+		length_dest++;
+	}
+
+	/* concatenate characters from src to dest */
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[length_dest++] = src[i];
+	}
+
+	/* Add terminating null byte to dest */
+	dest[length_dest] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
