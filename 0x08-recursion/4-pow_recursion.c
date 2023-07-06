e68af6467ec972aaafefe839ece78ee1a1e2c392
abdirashidabubakar7 <abdirashidabubakar7@penguin>
2023-07-07 01:50:09 +0300

    hope my mind is not gonna blow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion - function that returns the value of x power y
  *  @x: the base number
  *  @y: the exponent
- * 
+ *
  *  Return: returns negative if y is less than 0 and 1 if y is equal to 0
 */
 
@@ -16,12 +16,12 @@ int _pow_recursion(int x, int y)
 	}
 
 	/* returns 1 if y is equal to 0*/
-	if ( y == 0)
+	if (y == 0)
 	{
 		return (1);/* base case: any number to power 0 is 1*/
 	}
 
 	/* recursive call to power of x raised to (y -1)*/
-	return (x * _pow_recursion(x, y - 1)); 
+	return (x * _pow_recursion(x, y - 1));
 }
 
