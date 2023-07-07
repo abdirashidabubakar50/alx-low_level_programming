9edeefc8eebc8d60f917a5c60b7ea5832528bdb6
abdirashidabubakar7 <abdirashidabubakar7@penguin>
2023-07-07 21:52:26 +0300

    hell of a shit

---
+++ 0x09-static_libraries/0-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+
+int main(void)
+{
+  _putchar('H');
+  _putchar('\n');
+  return 0;
+}
---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* function prototypes */
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+
+#endif /* MAIN_H */
