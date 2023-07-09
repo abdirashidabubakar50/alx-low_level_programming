25c596a79b79c3be20a9da9079a5ef33d3b04364
abdirashidabubakar7 <abdirashidabubakar7@penguin>
2023-07-09 09:43:26 +0300

    unused variable is like an unwanted baby

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the name of the program.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line arguments.
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	
+	printf("%d\n", argc);
+	return (0);
+}
