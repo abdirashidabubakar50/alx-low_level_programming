53922a76cae5f1bb2dbf74061c368357741b7a1b
abdirashidabubakar7 <abdirashidabubakar7@penguin>
2023-07-11 00:03:29 +0300

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry into the main program
+ *
+ * The function mulitples two numbers into the terminal
+ *
+ * @argc: The number of comman-line arguments
+ * @argv: Pointer to the array of command-line argument
+ *
+ * Return: 0 on success and 1 on failure
+*/
+int main(int argc, char *argv[])
+{
+	int i, mul;
+	mul = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		mul = mul *  (atoi(argv[i]));
+	}
+	printf("%d\n", mul);
+	return (0);
+}
