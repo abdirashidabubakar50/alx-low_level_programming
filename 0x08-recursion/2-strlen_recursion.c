6db43c1b531894875fa6d9f851a786e7df8df033
Abdirashid Abubakar <abdirashidabubakar7@gmail.com>
2023-08-31 07:30:57 +0300

    well why is it that i am not able to see some small things

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - A function that prints the length of a given string
+ * @s: the string of which the length is going to be determined
+ * Return: returns the length of the string
+*/
+int _strlen_recursion(char *s)
+{
+	/*base case*/
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
