a8d217298641a4e6a87a315c883125ed57f70b96
Abdirashid Abubakar <abdirashidabubakar7@gmail.com>
2023-09-02 12:41:32 +0300

    I am not done yet

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,20 +11,16 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int sum;
+	int sum = 0;
 	int j = 0;
-	int num = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 	}
 
-	if (argc > 1)
-	{
 		for (i = 1; i < argc; i++)
 		{
-			sum = 0;
 			while (argv[i][j] != '\0')
 			{
 				if (!isdigit(argv[i][j]))
@@ -34,10 +30,8 @@ int main(int argc, char *argv[])
 				}
 				j++;
 			}
-			num = atoi(argv[i]);
-			sum += num;
+			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
-	}
 	return (0);
 }
