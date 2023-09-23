a5eaa61272e8ff0a93edc3ef9e7c9aeb92668f4c
Abdirashid Abubakar <abdirashidabubakar7@gmail.com>
2023-09-23 15:07:47 +0300

    well that was unexpected

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print - a funtion that prints just before the main function is executed
+*/
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
